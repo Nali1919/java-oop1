a1fdf713876386795e40804d7013fde142a23a5e
Nali1919 <qwerty1919979@gmail.com>
2023-01-27 15:27:42 +0100

    first commit

---
+++ src/jana60/bank/Conto.java
@@ -0,0 +1,18 @@
+package jana60.bank;
+
+public class Conto {
+	
+	int numeroConto;
+	String nomeProprietario;
+	int saldo;
+	
+	
+	public Conto(int numeroConto, String nomeProprietario, int saldo) {
+		super();
+		this.numeroConto = numeroConto;
+		this.nomeProprietario = nomeProprietario;
+		this.saldo = saldo;
+	}
+	
+
+}
---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,28 @@
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Random r = new Random();
+
+		int codiceProdotto = r.nextInt(1000);
+		
+
+		Prodotto i = new Prodotto(codiceProdotto, "Pasta", "Bel prodotto", 20, 22);
+		
+		
+		
+		System.out.println("Nome Prodtto:  " + i.nomeEsteso());
+		System.out.println("Il prezzo è:  " + i.prezzoSenzaIva());
+		System.out.println("Prezzo con iva: " +i.prezzoConIva());
+		
+	}
+	
+	
+	
+	
+	
+}
